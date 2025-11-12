6c79890cd318d353ec99e1dfe06bd9b5a1d29921
vikram-42-alla <allatrivikram@gmail.com>
2025-11-12 15:30:35 +0530

    fixed cors issue

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const axios = require("axios");
+const cors=require("cors")
 dotenv.config();
 const otpRoute=require("./Route/otpRoute");
 const connDb = require("./Utilities/db");
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.use("/otp",otpRoute)
 
 
