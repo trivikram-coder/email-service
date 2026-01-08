3dcb011725d891230057ea076a068db3f294f5c7
vikram-42-alla <allatrivikram@gmail.com>
2026-01-08 07:37:35 +0530

    commit

--- index.js
+++ index.js
@@ -12,14 +12,6 @@ app.use("/otp",otpRoute)
 
 
 
-// app.post("/send-mail", async (req, res) => {
-//   const { email } = req.body;
-//   if (!email) return res.status(400).json({ message: "Email is required" });
-//   const success = await sendMail(email);
-//   if (success) res.status(200).json({ message: "Email sent successfully" });
-//   else res.status(500).json({ message: "Failed to send email" });
-// });
-
 app.get("/", (req, res) => res.send("Mail API running"));
 connDb()
 app.listen(process.env.PORT, () => console.log(`Server on port ${process.env.PORT}`));
